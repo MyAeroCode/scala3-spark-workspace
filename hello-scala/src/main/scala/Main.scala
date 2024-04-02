328e75501c4fc4d2fed0933591188424c13cf2bf
Lutz <meteo_meteo@naver.com>
2024-04-02 18:14:37 +0900

    feat: 스칼라 초기 프로젝트 생성

--- hello-scala/src/main/scala/Main.scala
+++ hello-scala/src/main/scala/Main.scala
@@ -1,20 +1,5 @@
-import org.apache.spark.sql.SparkSession
+@main def hello(): Unit =
+  println("Hello world!")
+  println(msg)
 
-// export JAVA_OPTS='--add-exports java.base/sun.nio.ch=ALL-UNNAMED'
-
-@main def hello(): Unit = {
-  val spark = SparkSession.builder
-      .master("local")
-      .appName("HelloWorld")
-      .getOrCreate()
-      
-    println("First SparkContext:")
-    println("APP Name :"+spark.sparkContext.appName);
-    println("Deploy Mode :"+spark.sparkContext.deployMode);
-    println("Master :"+spark.sparkContext.master);
-
-    // val df = List("hello", "world").toDF  
-    // df.show()                             
-
-    spark.stop()
-}
+def msg = "I was compiled by Scala 3. :)"
